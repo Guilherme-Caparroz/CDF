5514a8b4356a32b9c31fd5c2e4a117bf41660faf
Guisantoss-gif <go9104390@gmail.com>
2026-08-17 23:40:36 +0000

    estrutura inicial

---
+++ app/2TelaDetalhe.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default function Tela2() { return (
+    <view>
+        <Pressable
+            onPress={ } style={style.headerbotao}>
+            <Text>Voltar</Text>
+        </Pressable> <text style={styles.headerpalavra}>Detalhes</text>
+    </view>
+)}
+
+<view>
+
+
+
+</view>
